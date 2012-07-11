4537dada97098f578f404487b6babcc66bb426b9
carlos barberis <carlos@silverstripe.com>
2012-07-12 11:04:38 +1200

    MINOR: add helper static function to check api credentials are set MINOR: check localhost is not part of url

--- code/UrlShortener.php
+++ code/UrlShortener.php
@@ -23,16 +23,20 @@ class UrlShortener extends Controller {
 	protected static function get_bitly_api_key() {
 		return self::$bitly_api_key;
 	}
+
+	static function ready_to_short() {
+		return (self::$bitly_api_key && self::$bitly_username);
+	}
 	
 	
 	function makeShortUrl($UrlSegment = null) {
 		if(!$UrlSegment) return '';
-		
+		if(strrpos($UrlSegment,'localhost') || !self::ready_to_short()) return $UrlSegment;
+
 		$req = new RestfulService("http://api.bitly.com/v3/shorten?login=".self::get_bitly_username()."&apiKey=".self::get_bitly_api_key()."&longUrl=".urlencode($UrlSegment)."&format=json",0);
-		
 		$response = $req->request();
-		
-		$short = json_decode($response->getBody(), true);		
+		$short = json_decode($response->getBody(), true);
+				
 		return (isset($short['data']['url'])) ? $short['data']['url'] : '';
 
 	}
